de8715d0991d80e13fa349933dbb5356eb72bc61
Ashmit Verma <a1verma8105@gmail.com>
2023-12-30 14:49:43 +0530

    landing page backend

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Pass(models.Model):
+    pass_icon=models.ImageField(upload_to="image_uploads/passicon/")
+
+    def __str__(self):
+        return str(self.pass_icon.name)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . models import Pass
 
 # Create your views here.
+def home(request):
+    passes=Pass.objects.all()
+    return render(request,"home.html",{'passes':passes})
