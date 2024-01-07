61ce958dc317329251dff69f5df2f9f0912d390f
Shaurya <shauryajain202@gmail.com>
2024-01-07 12:03:00 +0530

    integrate firebase database

--- main/views.py
+++ main/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from . models import Pass
+from django.core.mail import EmailMessage
+from django.core.mail import send_mail
+import random
+import pyrebase
+
+config = {
+   'apiKey': "AIzaSyCYBAFlRDVF_niUuOzk-dc4lo8v5XOg2cs",
+  'authDomain': "passportal-68a8a.firebaseapp.com",
+  'databaseURL': "https://passportal-68a8a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  'projectId': "passportal-68a8a",
+  'storageBucket': "passportal-68a8a.appspot.com",
+  'messagingSenderId': "28735319650",
+  'appId': "1:28735319650:web:5e1095ba7fdb52f4182b3b",
+}
+
+firebase=pyrebase.initialize_app(config)
+authe = firebase.auth()
+database=firebase.database()
+
+
 
 # Create your views here.
 def home(request):
     passes=Pass.objects.all()
-    return render(request,"home.html",{'passes':passes})
+    name = database.child('Data').child('Name').get().val()
+    return render(request,"home.html",{'passes':passes, 'name': name})
 
 def order_summary(request):
     # Fetch transaction data from Firebase
